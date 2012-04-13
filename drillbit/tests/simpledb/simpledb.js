3f72e5e0f196fd4f00f4964a6e66b3ba6e9ff129
Sumeet Saxena <sumeetsaxena@gmail.com>
2012-04-12 17:49:41 -0700

    Fixed drillbit tests file structure

--- drillbit/tests/simpledb/simpledb.js
+++ drillbit/tests/simpledb/simpledb.js
@@ -1,5 +1,9 @@
 
 describe("tests simple DB, although simpleMath for now", {
+
+  before_all: function() {
+        AWS = require('ti.aws');
+  },
   testAdd: function() {
     valueOf(1+1).shouldBe(2);
   },
