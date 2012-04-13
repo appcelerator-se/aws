1f4c865d0d7e84316ce03d045111533dbaaaf724
Sumeet Saxena <sumeetsaxena@gmail.com>
2012-04-13 10:35:28 -0700

    moved AWS initialization to before_all

--- drillbit/tests/aws/aws.js
+++ drillbit/tests/aws/aws.js
@@ -2,15 +2,7 @@
 describe("AWS Tests go here!", {
 
   before_all: function() {
-  },
-  testAdd: function() {
     AWS = require('ti.aws');
     AWS.authorize(Titanium.App.Properties.getString('aws-access-key-id'),Titanium.App.Properties.getString('aws-secret-access-key'));
-
-    valueOf(1+1).shouldBe(2);
-  },
- 
-  testSubtract: function() {
-    valueOf(2-1).shouldBe(1);
   }
 });
